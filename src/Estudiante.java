ad1d72ee119f3d2d4a7b74c240054365dc14bba0
Geronimo-9 <gmijaresa@unicartagena.edu.co>
2025-09-02 17:53:44 -0500

    Correccion del envio del ejercicio 1

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Estudiante {
+    private String nombre;
+    private int edad;
+
+
+    public Estudiante(String nombre, int edad){
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/Taller_2.java
+++ src/Taller_2.java
@@ -2,8 +2,12 @@ public class Taller_2{
     public static void main(String[] args) {
 
 
+Producto tottoPrime = new Producto("Totto", 49.90);
+Producto mentaHelada_Fresa = new Producto("mentahelada", 100);
 
-
+//Ejercicio 1
+mentaHelada_Fresa.mostrarProducto();
+tottoPrime.mostrarProducto();
 
 
 
