fb083090cab7d06adf26519cb4424c1bb03d5100
Geronimo-9 <gmijaresa@unicartagena.edu.co>
2025-09-02 15:57:34 -0500

    Clase producto con sus atributos

--- src/Producto.java
+++ src/Producto.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Producto {
+
+    private String nombre;
+    private double precio;
+
+    public Producto(){
+        this.nombre = nombre;
+        this.precio = precio;
+
+
+    }
+
+
+
+
+
+
+
 }
--- src/Taller_2.java
+++ src/Taller_2.java
@@ -2,7 +2,8 @@ public class Taller_2{
     public static void main(String[] args) {
 
 
-        System.out.println("listo");
+
+
 
 
 
